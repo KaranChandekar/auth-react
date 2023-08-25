260bb58bf153818a926a57e6f4afa7ede7bf8af5
Karan <karanchandekar431@gmail.com>
2023-08-26 03:17:55 +0530

    [feat]: add google font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
+    extend: {
+      height: {
+        "screen-minus-header": "calc(100vh - 80px)",
+      },
+    },
   },
   plugins: [],
-}
+};
