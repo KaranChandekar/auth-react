8df4e48325607f5405214be4c0eb782af99dcc17
Karan <karanchandekar431@gmail.com>
2023-08-26 03:18:46 +0530

    [feat]: create context

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Header from "./components/Header";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Logout from "./pages/Logout";
 import { Toaster } from "react-hot-toast";
 
 function App() {
@@ -10,9 +10,9 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/logout" element={<Logout />} />
       </Routes>
       <Toaster />
     </Router>
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React, { createContext, useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "./index.css";
 
 export const server = "https://node-auth-7lh3.onrender.com/api/v1";
 
