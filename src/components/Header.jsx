9b6e5dd2881254149984b7309d29e561542f7a8c
Karan <karanchandekar431@gmail.com>
2023-08-26 03:17:11 +0530

    [feat]: create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,45 +1,12 @@
-import { useContext } from "react";
-import { Link } from "react-router-dom";
-import { Context, server } from "../main";
-import axios from "axios";
-import { toast } from "react-hot-toast";
+import Logo from "../assets/logo.webp";
 
 const Header = () => {
-  const { isAuthenticated, setIsAuthenticated, loading, setLoading } =
-    useContext(Context);
-
-  const logoutHandler = async () => {
-    setLoading(true);
-
-    try {
-      const { data } = await axios.get(`${server}/users/logout`, {
-        withCredentials: true,
-      });
-      toast.success(data.message);
-      setIsAuthenticated(false);
-      setLoading(false);
-    } catch (error) {
-      toast.error(error.response.data.message);
-      console.error(error);
-      setIsAuthenticated(true);
-      setLoading(false);
-    }
-  };
-
   return (
-    <header className="header">
-      <div>
-        <h2>Untitled UI</h2>
+    <header className="mx-auto max-w-[1250px] h-20 flex justify-between items-center">
+      <div className="flex items-center gap-2">
+        <img src={Logo} alt="logo" className="w-10" />
+        <h2 className="text-xl font-medium">Salk AI</h2>
       </div>
-      <article>
-        {isAuthenticated ? (
-          <button disabled={loading} onClick={logoutHandler} className="btn">
-            Logout
-          </button>
-        ) : (
-          <Link to="/login">Login</Link>
-        )}
-      </article>
     </header>
   );
 };
