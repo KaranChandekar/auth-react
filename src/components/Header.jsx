e09ea8c53233dec9e4a02a3c56847ed7be7f4541
Karan <karanchandekar431@gmail.com>
2023-08-26 18:59:42 +0530

    [feat]: update app title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import Logo from "../assets/logo.webp";
 const Header = () => {
   return (
     <header className="w-full text-white bg-black items-center">
-      <div className="h-20 max-w-[1200px] m-auto flex justify-between items-center">
+      <div className="h-20 max-w-[1200px] m-auto flex justify-between items-center px-4">
         <div className="flex items-center gap-3">
           <img src={Logo} alt="logo" className="w-10" />
           <h2 className="text-xl font-semibold">Salk AI</h2>
